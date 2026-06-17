5f15553f150b569c885f717094b6f9e404ee4e29
Connor Bryant <connorjamesbryant7@gmail.com>
2026-06-16 21:46:40 -0400

    Footer update

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <!-- footer.php -->
 <footer>
     <div class="footer__main">
-        <p>&copy; <?php echo date('Y'); ?></p>
+        <p>&copy; <?php echo date('Y'); ?>
+        <?php echo esc_html( get_bloginfo( 'name' ) ); ?></p>
     </div>
 </footer>
 <?php wp_footer(); ?>
