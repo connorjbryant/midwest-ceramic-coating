38ee3c35ebc9683b3f5e6780a7ee121024bc0608
Connor Bryant <connorjamesbryant7@gmail.com>
2026-06-20 15:39:52 -0400

    404 page update

---
+++ 404.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<?php get_template_part( 'template-parts/404-content' ); ?>
+
+<?php get_footer(); ?>
---
+++ template-parts/404-content.php
@@ -0,0 +1,5 @@
+<main id="main" class="site-main error-404-content">
+    <h1>404</h1>
+    <p>Sorry, we couldn't find that page. <a href="/">Return Home?</a></p>
+    <?php get_search_form(); ?>
+</main>
