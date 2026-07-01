3936360bd272f6a8da4a5caa024f6ee9883a17b1
Connor Bryant <connorjamesbryant7@gmail.com>
2026-06-30 20:38:48 -0400

    Typo fix

--- functions.php
+++ functions.php
@@ -124,7 +124,7 @@ add_action('after_setup_theme', function () {
     }
 
     register_nav_menus([
-        'primary' => __('Primary Menu', 'midwest-cermaic-coating'),
+        'primary' => __('Primary Menu', 'midwest-ceramic-coating'),
     ]);
 });
 
